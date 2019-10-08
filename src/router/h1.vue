aa8163b044336dd399f2d7c0b86405c967ea4e3a
xiaoqi7777 <501798525@qq.com>
2019-10-08 20:03:05 +0800

    1

--- src/router/h1.vue
+++ src/router/h1.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-    h111111
+    测试
+    <el-select
+      v-model="value"
+      placeholder="请选择"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+    <el-button @click="btn">点击</el-button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      options: [{
+        value: '1',
+        label: '黄金糕'
+      }, {
+        value: '2',
+        label: '双皮奶'
+      }, {
+        value: '3',
+        label: '蚵仔煎'
+      }, {
+        value: '4',
+        label: '龙须面'
+      }, {
+        value: 5,
+        label: '北京烤鸭'
+      }],
+      value: 5
+    }
+  },
+  methods: {
+    btn () {
+      console.log('===', this.value)
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
