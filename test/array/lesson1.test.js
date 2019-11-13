6e0f2ef19c0895cc75bcbc6a7fa6691368ae840f
xiaoqi7777 <501798525@qq.com>
2019-11-13 14:08:42 +0800

    1

--- test/array/lesson1.test.js
+++
@@ -1,9 +0,0 @@
-import telComb from '../../code/arrary/lesson1'
-
-test('telComb:23', () => {
-    expect(telComb('23')).toEqual([
-        'ad', 'ae', 'af',
-        'bd', 'be', 'bf',
-        'cd', 'ce', 'cf'
-    ])
-})
--- test/array/lesson2.test.js
+++
@@ -1,5 +0,0 @@
-import telComb from '../../code/arrary/lesson2'
-
-test('telComb:[1,2,1,2]', () => {
-  expect(telComb([1, 2, 1, 2])).toBe(true)
-})
